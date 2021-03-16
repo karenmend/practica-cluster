cecf80fa77a192883d311ae317479f30722137ef
Karen Mendivil <karenlili14@gmail.com>
2021-03-16 16:50:09 -0700

    first commit

---
+++ proceso-master.js
@@ -0,0 +1,19 @@
+const cluster = require('cluster');
+console.log(`Master pid: ${process.pid}`);
+cluster.setupMaster({
+    exec : __dirname + '/servicio-receta.js'
+});
+
+cluster.fork();
+cluster.fork();
+
+cluster
+.on('disconnect', (worker) => {
+    console.log(`Desconectado: ${worker.id}`)
+})
+.on('exit', (worker, code, signal) =>{
+    console.log(`Salida; ${worker.id}`);
+})
+.on('listening', (worker, {adress, port}) => {
+    console.log(`Escuchando ${worker.id} en ${address}:${port}`)
+});
